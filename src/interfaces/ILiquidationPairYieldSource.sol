b103060eb373e56a0617ac1f815e30cabfa8bb45
Dylan DesRosier <dylan@pooltogether.com>
2023-01-16 12:08:55 -0500

    Contracts

---
+++ src/interfaces/ILiquidationPairYieldSource.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.17;
+
+// NOTE: This should be erc4626 or an adapter to it
+interface ILiquidationPairYieldSource {
+    function availableBalanceOf(address token) external returns (uint256);
+    function transfer(address token, address target, uint256 amount) external returns (bool);
+}
